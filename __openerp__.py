1fa23f5b01bcd50a3c9cbbd032df2e3d70e5bde8
Alejandro-Asr <alejandro@asr-oss.com>
2014-11-26 12:34:45 +0100

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2014 Alejandro Sánchez Ramírez (http://www.asr-oss.com)
+#                    Alejandro Sánchez Ramírez <alejandro@asr-oss.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Generación Facturae",
+    "version": "0.1",
+    "author": "Asr Oss",
+    "website": "http://www.asr-oss.com",
+    "category": "Accounting",
+    "contributors": [
+        "Alejandro Sánchez  <alejandro@asr-oss.com>",
+    ],
+    "description": """
+Generación fichero xml Facturae 3.2.1
+    """,
+    "depends": [
+        "account",
+        "l10n_es_partner_mercantil",
+    ],
+    "demo": [],
+    "data": [],
+    "installable": True,
+}
